bcd8efa2e0fc1d6a3a9dfe0601304a7f8e4298a7
Athroniaeth <pierre.chaumont@hotmail.fr>
2025-07-23 23:16:14 +0200

    ci: add bandit to ci cd to check security code

--- src/fastapi_docs_exception/__init__.py
+++ src/fastapi_docs_exception/__init__.py
@@ -3,11 +3,26 @@ from fastapi_docs_exception.handler import ExceptionResponseFactory
 
 def lint():  # pragma: no cover
     """Development lint function."""
-    import subprocess
+    import subprocess  # nosec: only calling static, internal commands
+    import shutil
 
-    subprocess.run(["ruff", "format", "."], check=True)
-    subprocess.run(["ruff", "check", ".", "--fix"], check=True)
-    subprocess.run(["ty", "check", "."], check=True)
+    ty = shutil.which("ty")
+    ruff = shutil.which("ruff")
+    bandit = shutil.which("bandit")
+
+    if (ty is None) or (ruff is None) or (bandit is None):
+        raise RuntimeError("Please install development dependencies: `uv sync --dev`")
+
+    list_commands = [
+        [ruff, "format", "."],
+        [ruff, "check", "--fix", "."],
+        [ty, "check", "."],
+        [bandit, "-c", "pyproject.toml", "-r", "src"],
+    ]
+
+    for command in list_commands:
+        print(f"Running command: `{' '.join(command)}`")
+        subprocess.run(command, check=True, shell=False)  # nosec: B603
 
 
 __all__ = ["ExceptionResponseFactory"]
